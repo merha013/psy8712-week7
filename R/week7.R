d7ec620bf0c6536a4e3a9c07eeba4a0340bbb8c2
Toni Merhar <merha013@umn.edu>
2024-03-10 17:56:40 -0500

    Corrected order of Male and Female, switched timeSpent to numeric (instead of difftime), and reduced figure generation process to include only a single pipe.

--- R/week7.R
+++ R/week7.R
@@ -1,8 +1,8 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-library(ggplot2)
-library (tidyverse)
+library (tidyverse) # includes ggplot2 and dplyr
 library(GGally)
+
 
 
 # Data Import and Cleaning
@@ -11,47 +11,46 @@ week7_tbl <- read.csv("../data/week3.csv", header=TRUE) %>%
   mutate(timeEnd = as.POSIXct(timeEnd)) %>%
   filter(q6==1) %>%
   select(-q6) %>%
-  mutate(timeSpent = difftime(timeEnd, timeStart, units = "mins")) %>%
-  mutate(gender = 
-           recode_factor(gender, F = "Female", M = "Male")) %>%
-  mutate(condition = 
-           recode_factor(condition, A = "Block A", B = "Block B", C = "Control"))
+  mutate(timeSpent = as.numeric(timeEnd-timeStart)) %>%
+  mutate(gender = recode_factor(gender, M = "Male", F = "Female")) %>%
+  mutate(condition = recode_factor(condition, A = "Block A", B = "Block B", C = "Control"))
+
+
 
 
 # Visualization
-week7_tbl %>%  
-  select(q1:q10) %>% 
+select(week7_tbl, q1:q10) %>% 
   ggpairs
-(week7_tbl %>%
-ggplot(aes(x=timeStart, y=q1)) +
+
+(ggplot(week7_tbl, aes(x=timeStart, y=q1)) +
   geom_point(shape = 20, size = 2) +
   guides(x=guide_axis("Date of Experiment"), y=guide_axis("Q1 Score"))) %>%
   ggsave("../figs/fig1.png", ., dpi=600, height = 4, width=8)
 
-(week7_tbl %>%
-  ggplot(aes(x=q1, y=q2, color=gender)) +
+
+(ggplot(week7_tbl, aes(x=q1, y=q2, color=gender)) +
   geom_jitter(width=.08, height =.08, shape = 20, size =2) +
-  labs(color = "Participant Gender")) %>%   # not sure how to list Male first
+  labs(color = "Participant Gender")) %>%
   ggsave("../figs/fig2.png", ., dpi=600, height = 4, width=8)
-(week7_tbl %>%
-  ggplot(aes(x=q1, y=q2)) +
+
+(ggplot(week7_tbl, aes(x=q1, y=q2)) +
   geom_jitter(width=.08, height =.08, shape = 20, size =2) +
   guides(x=guide_axis("Score on Q1"), y=guide_axis("Score on Q2")) +
-  facet_grid(.~gender)) %>%  # not sure how to list Male first
+  facet_grid(.~gender)) %>%
   ggsave("../figs/fig3.png", ., dpi=600, height = 4, width=8)
 
-(week7_tbl %>%
-    ggplot(aes(x=gender, y=timeSpent)) + # Is it better to have timeSpent be continuous rather than difftime?
-    geom_boxplot() + # not sure how to list Male first
+
+(ggplot(week7_tbl, aes(x=gender, y=timeSpent)) +
+    geom_boxplot() +
     guides(x=guide_axis("Gender"), y=guide_axis("Time Elapsed (mins)"))) %>%
   ggsave("../figs/fig4.png", ., dpi=600, height = 4, width=8)
 
-(week7_tbl %>%
-    ggplot(aes(x=q5, y=q7, color=condition)) +
+
+(ggplot(week7_tbl, aes(x=q5, y=q7, color=condition)) +
     geom_jitter(width=.08, height =.08, shape = 20, size =2) +
     labs(color = "Experimental Condition") +
     guides(x=guide_axis("Score on Q5"), y=guide_axis("Score on Q7")) +
     geom_smooth(method = "lm", se=F) +
     theme(legend.position = "bottom", 
-          legend.background = element_rect(fill = grey(0.875)))) %>% # hex_code "#DFDFDF"
+          legend.background = element_rect(fill = grey(0.875)))) %>% # hex_code = "#DFDFDF"
   ggsave("../figs/fig5.png", ., dpi=600, height = 4, width=8)
